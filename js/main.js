24b8375ebe0f8dae7306ab992b40d412ea9a4a08
FranMayer <francomayer96@outlook.com>
2023-03-21 23:55:26 -0300

    Modificando metodos en js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function consultarPrecios() {
+/*function consultarPrecios() {
   let continuar = true;
   
   while (continuar) {
@@ -74,3 +74,39 @@ function consultarPrecios() {
 
 // Ejecutamos la función al cargar la página
 consultarPrecios();
+*/
+
+const coches = [
+
+  {id: "001", nombre: "Footwork FA16", precio: "2750", disponibilidad: "no"},
+  {id: "002", nombre: "Bennetton B139", precio: "2800", disponibilidad: "si"},
+  {id: "003", nombre: "Bennetton B592", precio: "300", disponibilidad: "no"},
+  {id: "004", nombre: "Forti FG01-95", precio: "2920", disponibilidad: "si"},
+  {id: "005", nombre: "Ferrari F643", precio: "3000", disponibilidad: "si"},
+  {id: "006", nombre: "Mclaren MP4/30", precio: "2500", disponibilidad: "si"},
+  {id: "007", nombre: "Jordan EJ12", precio: "2800", disponibilidad: "si"},
+  {id: "008", nombre: "Jordan EJ12b", precio: "2850", disponibilidad: "si"},
+  {id: "009", nombre: "Mclaren MP4/18", precio: "2100", disponibilidad: "si"},
+  {id: "010", nombre: "Mercedes Mclaren MP4/29", precio: "2700", disponibilidad: "si"},
+  {id: "011", nombre: "Red Bull C14 ", precio: "3500", disponibilidad: "si"},
+  {id: "012", nombre: "Renault R25", precio: "3650", disponibilidad: "si"},
+
+];
+
+const idCoche = prompt("Introduce el ID del coche a buscar:");
+
+const cocheEncontrado = coches.find((coche) => coche.id === idCoche);
+
+if (cocheEncontrado) {
+  if (cocheEncontrado.disponibilidad === "si") {
+    alert(`El coche ${cocheEncontrado.nombre} está disponible y su precio es $ ${cocheEncontrado.precio} pesos👌`);
+  } else {
+    alert(`El coche ${cocheEncontrado.nombre} no está disponible en este momento😥`);
+  }
+} else {
+  alert("El ID del coche introducido no es correcto, por favor ingrese un numero valido del (001 al 0012)❌");
+}
+
+const cochesDisponibles = coches.filter((coche) => coche.disponibilidad === "si");
+
+alert("")
