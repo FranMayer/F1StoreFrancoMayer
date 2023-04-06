d32be0468455c54dabbaaec3a9f79c2b96eae7f7
FranMayer <francomayer96@outlook.com>
2023-04-06 01:28:40 -0300

    Agregando funcion para agregar al menu desplegable

--- js/main.js
+++ js/main.js
@@ -1,30 +1,58 @@
-const coches = [
-
-  {id: 001, nombre: "Footwork FA16", precio: 2750, disponibilidad: "no"},
-  {id: 002, nombre: "Bennetton B139", precio: 2800, disponibilidad: "si"},
-  {id: 003, nombre: "Bennetton B592", precio: 3000, disponibilidad: "no"},
-  {id: 004, nombre: "Forti FG01-95", precio: 2920, disponibilidad: "si"},
-  {id: 005, nombre: "Ferrari F643", precio: 3000, disponibilidad: "si"},
-  {id: 006, nombre: "Mclaren MP4/30", precio: 2500, disponibilidad: "si"},
-  {id: 007, nombre: "Jordan EJ12", precio: 2800, disponibilidad: "no"},
-  {id: 008, nombre: "Jordan EJ12b", precio: 2850, disponibilidad: "no"},
-  {id: 009, nombre: "Mclaren MP4/18", precio: 2100, disponibilidad: "si"},
-  {id: 010, nombre: "Mercedes Mclaren MP4/29", precio: 2700, disponibilidad: "no"},
-  {id: 011, nombre: "Red Bull C14 ", precio: 3500, disponibilidad: "si"},
-  {id: 012, nombre: "Renault R25", precio: 3650, disponibilidad: "si"},
-]; 
+const productos = [
+  {id: "001", nombre: "Footwork FA16", precio: 2750, disponibilidad: "no"},
+  {id: "002", nombre: "Bennetton B139", precio: 2800, disponibilidad: "si"},
+  {id: "003", nombre: "Bennetton B592", precio: 3000, disponibilidad: "no"},
+  {id: "004", nombre: "Forti FG01-95", precio: 2920, disponibilidad: "si"},
+  {id: "005", nombre: "Ferrari F643", precio: 3000, disponibilidad: "si"},
+  {id: "006", nombre: "Mclaren MP4/30", precio: 2500, disponibilidad: "si"},
+  {id: "007", nombre: "Jordan EJ12", precio: 2800, disponibilidad: "no"},
+  {id: "008", nombre: "Jordan EJ12b", precio: 2850, disponibilidad: "no"},
+  {id: "009", nombre: "Mclaren MP4/18", precio: 2100, disponibilidad: "si"},
+  {id: "010", nombre: "Mercedes Mclaren MP4/29", precio: 2700, disponibilidad: "no"},
+  {id: "011", nombre: "Red Bull C14 ", precio: 3500, disponibilidad: "si"},
+  {id: "012", nombre: "Renault R25", precio: 3650, disponibilidad: "si"},
+];
 
 //Variable para el carrito 
 const carrito = [];
 
 function agregarProductoAlCarrito(evento) {
-  const idProducto = evento.target.dataset.id;
-  const producto = coches.find(producto => producto.id === idProducto);
-  carrito.push(producto);
+  if (evento) {
+    const idProducto = evento.target.dataset.id;
+    const producto = productos.find(producto => producto.id === idProducto);
+    carrito.push(producto);
+    const nuevoElemento = crearElementoEnMenu(producto);
+    document.getElementById("menu").appendChild(nuevoElemento);
+  }
 }
 
-const botones = document.getElementById("boton");
-botones.addEventListener("click", agregarProductoAlCarrito);
+const botones = document.querySelectorAll(".boton");
+botones.forEach(boton => {
+  boton.addEventListener("click", agregarProductoAlCarrito);
+});
+
+function crearElementoEnMenu(coche) {
+  const nuevoElemento = document.createElement("div");
+  nuevoElemento.classList.add("canvas");
+  
+  const nombreCoche = document.createElement("p");
+  nombreCoche.textContent = coche.nombre;
+  
+  const precioCoche = document.createElement("p");
+  precioCoche.textContent = `Precio: $${coche.precio}`;
+  
+  const idCoche = document.createElement("p");
+  idCoche.textContent = `ID: ${coche.id}`;
+
+  nuevoElemento.appendChild(nombreCoche);
+  nuevoElemento.appendChild(precioCoche);
+  nuevoElemento.appendChild(idCoche);
+  
+  return nuevoElemento;
+}
+
+
+
 
 
 
